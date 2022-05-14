fa7d43d2d23f7b1ca7a2fdc6c9a660f621ea65e1
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-05-14 14:37:25 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,10 @@ app.use(cookieParser());
 //route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 app.use("/api/v1", product);
 app.use("/api/v1/", user);
+app.use("/api/v1", order);
 
 // middleware for errors
 app.use(errorMiddleware);
